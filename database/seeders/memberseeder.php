503e5dd9f25f7b889d6f9213a70cc3132c09f8c5
Isiaka ibrahim <codemaskingz@gmail.com>
2022-06-08 08:55:07 +0100

    something

--- database/seeders/memberseeder.php
+++ database/seeders/memberseeder.php
@@ -18,11 +18,11 @@ class memberseeder extends Seeder
     public function run()
     {
         //
-        DB::table('studentdata')->insert([
-            'lastname'=>Str::random(10),
-            'firstname'=>Str::random(5),
-            'username'=>Str::random(10),
-            'address'=>Str::random(10)
+        DB::table('studentdatas')->insert([
+            'LastName'=>Str::random(10),
+            'FirstName'=>Str::random(10),
+            'Email'=>Str::random(10).' @gmail.com',
+            'Address'=>Str::random(10)
         ]);
     }
 }
